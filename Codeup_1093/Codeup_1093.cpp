9168812d4e16e13101e5a8f8dc283b91f5404019
leeejin <asd72621@naver.com>
2020-02-04 22:19:46 +0900

    Codeup

---
+++ Codeup_1093/Codeup_1093.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,t;
+	cin>>n;
+	int arr[24]={0};
+	for(int i=1; i<=n; i++)
+	{
+		cin>>t;
+		arr[t] +=1;
+	}
+	for(int i=1; i<=23; i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	return 0;
+}
